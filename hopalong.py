0185e56898df893e76f39a2867c8705bd0735d16
ratwolfzero <ratwolf@duck.com>
2024-05-21 05:48:57 +0200

    modified:   hopalong.py     seperate definition of image_size     increased image_size

--- hopalong.py
+++ hopalong.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 
+image_size=(10000, 10000)
 
-def hopalong(num, a, b, c, image_size=(1001, 1001)):
+def hopalong(num, a, b, c, image_size):
     x, y = 0, 0
     u = np.zeros(num)
     v = np.zeros(num)
@@ -23,8 +24,8 @@ def hopalong(num, a, b, c, image_size=(1001, 1001)):
     img_width, img_height = image_size
     img = np.zeros((img_height, img_width))
 
-    px = ((u - min_x) / (max_x - min_x) * (img_width - 1)).astype(int)
-    py = ((v - min_y) / (min_y - max_y) * (img_height - 1)).astype(int)
+    px = ((u - min_x) / (max_x - min_x) * (img_width-1)).astype(int)
+    py = ((v - min_y) / (min_y - max_y) * (img_height-1)).astype(int)
     img[py, px] = 1
 
     plt.figure(figsize=(8, 8))
@@ -32,10 +33,9 @@ def hopalong(num, a, b, c, image_size=(1001, 1001)):
     plt.title(f"Hopalong Attractor\nParams: a={a}, b={b}, c={c}, num={num}")
     plt.show()
 
-
 print("Input the parameters a, b, c (e.g., -1.7 -0.3 0.7) and the number of iterations num (e.g., 1000000 or 1_000_000)")
 a = float(input('a? '))
 b = float(input('b? '))
 c = float(input('c? '))
 num = int(input('num? '))
-hopalong(num, a, b, c)
+hopalong(num, a, b, c, image_size)

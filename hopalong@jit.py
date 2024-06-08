c11cf78d447099f5c33687ebdce31bc498ad8a23
ratwolfzero <ratwolf@duck.com>
2024-06-08 08:33:14 +0200

    new file:   examples/example_17.png hopalong@jit.py imshow extend

--- hopalong@jit.py
+++ hopalong@jit.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numba import jit
+
+# Use TkAgg backend for matplotlib
 import matplotlib
 matplotlib.use('TkAgg')
 
@@ -35,7 +37,7 @@ def hopalong_plot(points, a, b, c, num, image_size):
     img[py, px] = 1
 
     plt.figure(figsize=(10, 10))
-    plt.imshow(img, cmap='inferno')
+    plt.imshow(img, cmap='inferno',extent=[min_x, max_x, min_y, max_y])
     plt.title(
         f"Hopalong Attractor@ratwolf2024\nParams: a={a}, b={b}, c={c}, num={num:,}")
     plt.show()
@@ -53,18 +55,15 @@ def get_validated_input(prompt, input_type=float, check_non_zero=False, check_nu
         user_input = input(prompt)
         try:
             value = input_type(user_input)
-        except ValueError:
-            print(f"Invalid input. Please enter a valid {
-                  input_type.__name__} value.")
-            continue
-
-        if check_non_zero and value == 0:
-            print("Invalid input. The value cannot be zero.")
-
-        if check_num and value < 1_000_000:
-            print("inappropriate input. The value for num should be at least 1_000_000.")
-        else:
+            if check_non_zero and value == 0:
+                print("Invalid input. The value cannot be zero.")
+                continue
+            if check_num and value < 1_000_000:
+                print("Inappropriate input. The value for num should be at least 1,000,000.")
+                continue
             return value
+        except ValueError:
+            print(f"Invalid input. Please enter a valid {input_type.__name__} value.")
 
 
 def main():

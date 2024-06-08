3f45764f0b7e93a1cbe273758a8ad6116f1e2df9
ratwolfzero <ratwolf@duck.com>
2024-06-08 08:45:07 +0200

    modified:   multi line comment

--- hopalong@jit.py
+++ hopalong@jit.py
@@ -37,7 +37,7 @@ def hopalong_plot(points, a, b, c, num, image_size):
     img[py, px] = 1
 
     plt.figure(figsize=(10, 10))
-    plt.imshow(img, cmap='inferno',extent=[min_x, max_x, min_y, max_y])
+    plt.imshow(img, cmap='inferno', extent=[min_x, max_x, min_y, max_y])
     plt.title(
         f"Hopalong Attractor@ratwolf2024\nParams: a={a}, b={b}, c={c}, num={num:,}")
     plt.show()
@@ -45,8 +45,10 @@ def hopalong_plot(points, a, b, c, num, image_size):
 
 def hopalong(num, a, b, c, image_size):
     points = hopalong_compute(num, a, b, c).astype(np.float32)
-    # Note: The points variable in the hopalong_compute function and the points variable in the hopalong function are different local variables.
-    # They exist in different scopes and have their own lifetimes!
+    """
+    Note: The points variable in the hopalong_compute function and the points variable in the hopalong function are different local variables.
+    They exist in different scopes and have their own lifetimes!
+    """
     hopalong_plot(points, a, b, c, num, image_size)
 
 
@@ -59,11 +61,13 @@ def get_validated_input(prompt, input_type=float, check_non_zero=False, check_nu
                 print("Invalid input. The value cannot be zero.")
                 continue
             if check_num and value < 1_000_000:
-                print("Inappropriate input. The value for num should be at least 1,000,000.")
+                print(
+                    "Inappropriate input. The value for num should be at least 1,000,000.")
                 continue
             return value
         except ValueError:
-            print(f"Invalid input. Please enter a valid {input_type.__name__} value.")
+            print(f"Invalid input. Please enter a valid {
+                  input_type.__name__} value.")
 
 
 def main():

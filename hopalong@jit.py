4f7c4d001d4d2c1356573348e52a3d74e5ada534
ratwolfzero <ratwolf@duck.com>
2024-06-08 22:42:12 +0200

    modified:   README.md

--- hopalong@jit.py
+++ hopalong@jit.py
@@ -9,7 +9,7 @@ matplotlib.use('TkAgg')
 
 @jit(nopython=True)
 def hopalong_compute(num, a, b, c):
-   # Computes the points for the Hopalong attractor. 
+   # Computes the points for the Hopalong attractor.
     points = np.empty((num, 2), dtype=np.float32)
     x, y = 0.0, 0.0
 

ec3ceba0d284ddf56fac0e533f77f342332c0acd
ratwolfzero <ratwolf@duck.com>
2024-06-11 21:59:00 +0200

    new file:   examples/advanced version/Figure_16.png new file:   examples/advanced version/Figure_17.png
    readme

--- hopalong_advanced.py
+++ hopalong_advanced.py
@@ -50,6 +50,7 @@ def hopalong_plot(points, a, b, c, num, image_size):
                extent=[min_x, max_x, min_y, max_y])
     plt.title(
         f"Hopalong Attractor@ratwolf2024\nParams: a={a}, b={b}, c={c}, num={num:_}")
+
     plt.show()
 
 

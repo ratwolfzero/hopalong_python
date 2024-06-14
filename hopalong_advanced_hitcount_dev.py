0a0c2db986918fdc44fde707b2a9b88a2c59f4f7
Ralf <ratwolf@duck.com>
2024-06-14 17:19:43 +0200

    new file:   hopalong_advanced_hitcount_dev.py streamline hopalong_plot function, which was a bit chaotic meanwhile due to the statistics calculated

---
+++ hopalong_advanced_hitcount_dev.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import jit
+
+# Use TkAgg backend for matplotlib
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+@jit(nopython=True)
+def hopalong_compute(num, a, b, c):
+   # Computes the points for the Hopalong attractor.
+    points = np.zeros((num, 2), dtype=np.float32)
+    x, y = 0.0, 0.0
+
+    for i in range(num):
+
+        points[i] = x, y
+        xx, yy = y - np.sign(x) * np.sqrt(abs(b * x - c)), a - x
+        x, y = xx, yy
+
+    return points
+
+
+@jit(nopython=True)
+def pixels_and_hit_count(img, px, py):
+    # set pixels and track their density (used for color sheme cmap)
+    for px_i, py_i in zip(px, py):
+        img[py_i, px_i] += 1
+    return img
+
+
+def calculate_pixels_points(points, image_size):
+    # calculate pixels based on points for image size
+    min_x, max_x = np.min(points[:, 0]), np.max(points[:, 0])
+    min_y, max_y = np.min(points[:, 1]), np.max(points[:, 1])
+
+    img_width, img_height = image_size
+
+    px = ((points[:, 0] - min_x) / (max_x - min_x) * (img_width - 1)).astype(np.int16)
+    py = ((points[:, 1] - min_y) / (max_y - min_y) * (img_height - 1)).astype(np.int16)
+
+    return px, py
+
+def plot_attractor_image(img, colormap, extents, params, size=(8,8)):
+    plt.figure(figsize=size)
+    plt.imshow(img, cmap=colormap, origin='lower', extent=extents)
+    plt.title("Hopalong Attractor @ratwolf2024\nParams: a={a}, b={b}, c={c}, num={num:_}".format(**params))
+    
+def plot_intensity_distribution(img, colormap, size=(10,8), scale='log'):
+    hit, count = np.unique(img[img!=0], return_counts=True)
+    total = sum(j for i, j in zip(hit, count))
+    img_pixel = np.prod(img.shape)
+    hit_ratio = '{:02.2f}'.format(total / img_pixel * 100)
+    plt.figure(figsize=size)
+    plt.xlabel('# of hits (n)',fontsize=10)
+    plt.ylabel('# of pixels hit n-times',fontsize=10)
+    plt.title(f'Distribution of pixel hit count. In total {total} pixels of {img_pixel} = {hit_ratio}% have been hit',fontsize=12)
+    plt.scatter(hit, count, s=count/10, c=hit, cmap=colormap)
+    plt.xscale(scale)
+    plt.yscale(scale)
+    plt.xlim(left=0.8) 
+    plt.ylim(bottom=1) 
+    
+
+def hopalong_plot(points, a, b, c, num, image_size):
+    # Plots the points of the Hopalong attractor.
+    color_map='hot'
+    
+    px, py = calculate_pixels_points(points, image_size)
+    img = pixels_and_hit_count(np.zeros(image_size, dtype=np.int16), px, py)
+    
+    min_x, max_x = np.min(points[:,0]), np.max(points[:,0])
+    min_y, max_y = np.min(points[:,1]), np.max(points[:,1])
+    extents=[min_x, max_x, min_y, max_y]
+    
+    params = {'a': a, 'b': b, 'c': c, 'num': num}
+    
+    plot_attractor_image(img, color_map, extents, params)
+    plot_intensity_distribution(img, color_map)
+    
+    plt.show()
+    
+
+def hopalong(num, a, b, c, image_size):
+    # Computes and plots the Hopalong attractor.
+    points = hopalong_compute(num, a, b, c).astype(np.float32)
+    hopalong_plot(points, a, b, c, num, image_size)
+
+
+def get_validated_input(prompt, input_type=float, check_non_zero=False):
+    # Prompts the user for input and validates it.
+    while True:
+        user_input = input(prompt)
+        try:
+            value = input_type(user_input)
+            if check_non_zero and value == 0:
+                print("Invalid input. The value cannot be zero.")
+                continue
+            return value
+        except ValueError:
+            print(f"Invalid input. Please enter a valid {
+                  input_type.__name__} value.")
+
+
+def main():
+   # Main function to run the Hopalong attractor generation.
+    image_size = 1000, 1000
+
+    a = get_validated_input(
+        'Enter a non-zero float value for "a": ', float, check_non_zero=True)
+    b = get_validated_input('Enter a float value for "b": ', float)
+    c = get_validated_input('Enter a float value for "c": ', float)
+    num = get_validated_input(
+        'Enter an integer value for "num": ', int)
+
+    hopalong(num, a, b, c, image_size)
+
+
+if __name__ == "__main__":
+    main()

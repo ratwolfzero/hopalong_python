1c5ac80418c0cda8bcc117a5eaa1e841a84b539b
Ralf <ratwolf@duck.com>
2024-06-29 04:33:55 +0200

    modified:   Development/hopalong_advanced_dev.py modified:   Documentation/hopalong_advanced_doc.py modified:   Documentation/html/hopalong_advanced_doc.html modified:   hopalong_advanced.py

--- Development/hopalong_advanced_dev.py
+++ Development/hopalong_advanced_dev.py
@@ -43,7 +43,7 @@ def map_attractor_points_to_image_pixels(points, image_size):
 @njit(parallel=True)
 def generate_image_and_pixel_counts(img, px, py):
     """
-    Populate the image array with hit counts for each pixel. The pixel Color is defined by color_map
+    Populate the image array with hit counts for each pixel. The pixel color is defined by color_map
     """
     # use prange for parallel loop
     for i in prange(len(px)):

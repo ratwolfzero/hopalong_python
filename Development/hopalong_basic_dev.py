4436fc3838db2e01e27e64c41b641f6882a808f8
Ralf <ratwolf@duck.com>
2024-07-05 21:10:25 +0200

    modified:   Development/hopalong_basic_dev.py

--- Development/hopalong_basic_dev.py
+++ Development/hopalong_basic_dev.py
@@ -24,8 +24,8 @@ def custom_sign(x):
     
 
 @njit
-def calculate_attractor_points(a, b, c, num):
-    # Compute points of the hopalong attractor
+def hopalong_trajectory_simulation(a, b, c, num):
+    # Simulates the trajectory of the Hopalong Attractor
     """
     Remark: Parallel options cannot be used here due to the cross-iteration dependency.
     points[i+1] cannot be calculated without first computing points[i]
@@ -43,8 +43,8 @@ def calculate_attractor_points(a, b, c, num):
 
 
 @njit(parallel=True)
-def map_attractor_points_to_image_pixels(points, image_size):
-    # map attractor points to corresponding image pixel coordinates
+def map_points_to_image(points, image_size):
+    # Maps the trajectory points to an image grid
     img_width, img_height = image_size
 
     min_x, max_x = np.min(points[:, 0]), np.max(points[:, 0])
@@ -58,7 +58,7 @@ def map_attractor_points_to_image_pixels(points, image_size):
 
 @njit(parallel=True)
 def generate_image(img, px, py):
-# Populate image array. Each hit point gets a unique value based on the index
+# Populates an image array with trajectory points. Each point gets a unique value based on the index
     
     # use prange for parallel loop
     for i in prange(len(px)):
@@ -68,8 +68,8 @@ def generate_image(img, px, py):
 
 
 def prepare_plot_data(points, a, b, c, num, image_size):
-    # Process attractor points and prepare data for visualization
-    px, py, min_x, max_x, min_y, max_y = map_attractor_points_to_image_pixels(points, image_size)
+    # Processes trajectory points and prepares data for visualization
+    px, py, min_x, max_x, min_y, max_y = map_points_to_image(points, image_size)
     img = generate_image(np.zeros(image_size, dtype=np.int32), px, py)
 
     extents = [min_x, max_x, min_y, max_y]
@@ -78,8 +78,8 @@ def prepare_plot_data(points, a, b, c, num, image_size):
     return img, extents, params
 
 
-def plot_attractor(img, extents, params, color_map):
-    # plot image and render the hopalong attractor on a given axis with the specified colormap
+def render_trajectory_image(img, extents, params, color_map):
+    # Renders the trajectory of the Hopalong Attractor as an image
     plt.figure(figsize=(8, 8))
     plt.imshow(img, origin="lower", cmap=color_map, extent=extents)
     plt.title(
@@ -112,21 +112,21 @@ def get_user_inputs():
     return a, b, c, num
 
 
-def generate_and_plot_attractor(a, b, c, num, image_size, color_map):
-    # Generate and process attractor points and create visualizations
-    points = calculate_attractor_points(a, b, c, num)
+def simulate_trajectory_and_render_trajectory_image(a, b, c, num, image_size, color_map):
+    # simulate hopalong attractor trajetory and create visualizations
+    points = hopalong_trajectory_simulation(a, b, c, num)
     img, extents, params = prepare_plot_data(points, a, b, c, num, image_size)
-    plot_attractor(img, extents, params, color_map)
+    render_trajectory_image(img, extents, params, color_map)
 
 
 def main():
-    #Entry point: Coordinate user input, processing of attractor points and visualization generation
+    #Entry point: Coordinate user input, processing of attractor trajectory and visualization generation
     image_size = 1000, 1000
     color_map = 'inferno'
  
     a, b, c, num = get_user_inputs()
 
-    generate_and_plot_attractor(a, b, c, num, image_size, color_map)
+    simulate_trajectory_and_render_trajectory_image(a, b, c, num, image_size, color_map)
 
 
 if __name__ == "__main__":

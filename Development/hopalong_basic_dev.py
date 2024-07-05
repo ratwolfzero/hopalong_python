b57838a381f45c57d48d69b3a5b6f7e4de166cd3
Ralf <ratwolf@duck.com>
2024-07-05 21:16:08 +0200

    modified:   Development/hopalong_basic_dev.py

--- Development/hopalong_basic_dev.py
+++ Development/hopalong_basic_dev.py
@@ -104,7 +104,7 @@ def get_validated_input(prompt, input_type=float, check_non_zero=False):
 
 
 def get_user_inputs():
-    # Collect input parameters from the user for attractor generation
+    # Collect input parameters from the user for hopalong attractor trajetory generation
     a = get_validated_input('Enter a non-zero float value for "a": ')#, float, check_non_zero=True)
     b = get_validated_input('Enter a float value for "b": ', float)
     c = get_validated_input('Enter a float value for "c": ', float)

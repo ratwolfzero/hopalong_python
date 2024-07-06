faa140878836b1c0c7b947cd59c71612ce2c0a3a
Ralf <ratwolf@duck.com>
2024-07-06 03:56:23 +0200

    modified:   Development/hopalong_advanced_dev.py modified:   Documentation/hopalong_advanced_doc.py deleted:    Documentation/html/hopalong_advanced_doc.html modified:   hopalong_advanced.py

--- Development/hopalong_advanced_dev.py
+++ Development/hopalong_advanced_dev.py
@@ -22,8 +22,8 @@ def custom_sign(x):
         return -1.0
 
 @njit
-def calculate_attractor_points(a, b, c, num):
-    # Compute points of the hopalong attractor
+def hopalong_trajectory_simulation(a, b, c, num):
+    # Simulates the trajectory of the Hopalong Attractor
     """
     Remark: Parallel options cannot be used here due to the cross-iteration dependency.
     points[i+1] cannot be calculated without first computing points[i]
@@ -41,8 +41,8 @@ def calculate_attractor_points(a, b, c, num):
 
 
 @njit(parallel=True)
-def map_attractor_points_to_image_pixels(points, image_size):
-    # map attractor points to corresponding image pixel coordinates
+def map_points_to_image(points, image_size):
+    # Maps the trajectory points to an image grid
     img_width, img_height = image_size
 
     min_x, max_x = np.min(points[:, 0]), np.max(points[:, 0])
@@ -55,8 +55,8 @@ def map_attractor_points_to_image_pixels(points, image_size):
 
 
 @njit(parallel=True)
-def generate_image_and_pixel_counts(img, px, py):
-# Populate image array and aggregate hit counts per pixel from attractor points
+def generate_image(img, px, py):
+# Populates an image array with trajectory points. Each point gets a unique value based on the hit count
     
     # use prange for parallel loop
     for i in prange(len(px)):
@@ -65,14 +65,26 @@ def generate_image_and_pixel_counts(img, px, py):
     return img
 
 
-def plot_attractor(ax, img, colormap, extents, params):
-    # plot image and render the hopalong attractor on a given axis with the specified colormap
+def prepare_plot_data(points, a, b, c, num, image_size):
+    # Processes trajectory points, hit metrics and prepares data for visualization
+    px, py, min_x, max_x, min_y, max_y = map_points_to_image(points, image_size)
+    img = generate_image(np.zeros(image_size, dtype=np.int32), px, py)
+    hit_metrics = calculate_hit_metrics(img) 
+
+    extents = [min_x, max_x, min_y, max_y]
+    params = {'a': a, 'b': b, 'c': c, 'num': num}
+    
+    return img, extents, params, hit_metrics  
+
+
+def render_trajectory_image(ax, img, colormap, extents, params):
+    # Renders the trajectory of the Hopalong Attractor as an image
     ax.imshow(img, cmap=colormap, origin='lower', extent=extents)
     ax.set_title("Hopalong Attractor@ratwolf@2024\nParams: a={a}, b={b}, c={c}, num={num:_}".format(**params))
 
 
 def calculate_hit_metrics(img):
-    # Analyze and summarize hit metrics from the attractor image
+    # Analyze and summarize hit metrics from the hopalong trajectory image
     hit, count = np.unique(img[img > 0], return_counts=True)
     max_count_index = np.argmax(count)
     hit_for_max_count = hit[max_count_index]
@@ -96,7 +108,7 @@ def calculate_hit_metrics(img):
 
 
 def plot_hit_metrics(ax, hit_metrics, scale='log'):
-    # Visualize the distribution of hit counts on pixels in the attractor image
+    # Visualize the distribution of hit counts on pixels in the hopalong trajectory image
     ax.plot(hit_metrics["hit"], hit_metrics["count"], 'o-', color="navy", markersize=1,linewidth=0.6)
     ax.set_xlabel('# of hits (n)')
     ax.set_ylabel('# of pixels hit n-times')
@@ -114,26 +126,14 @@ def plot_hit_metrics(ax, hit_metrics, scale='log'):
     ax.set_ylim(bottom=0.9) # y coordinate: Better resolution and display of values ​​at the bottom end of the scale
     ax.set_facecolor("lightgrey")
     ax.grid(True, which="both")
-
-
-def prepare_plot_data(points, a, b, c, num, image_size):
-    # Process the attractor points, hit metrics and prepare data for plotting
-    px, py, min_x, max_x, min_y, max_y = map_attractor_points_to_image_pixels(points, image_size)
-    img = generate_image_and_pixel_counts(np.zeros(image_size, dtype=np.int32), px, py)
-    hit_metrics = calculate_hit_metrics(img) 
-
-    extents = [min_x, max_x, min_y, max_y]
-    params = {'a': a, 'b': b, 'c': c, 'num': num}
-    
-    return img, extents, params, hit_metrics  
     
 
 def create_plots(img, extents, params, hit_metrics, color_map):    
-    # Generate attractor and hit metrics plots
+    # Display hopalong trajetory image and hit metrics plot
     fig = plt.figure(figsize=(18, 8))
 
     ax1 = fig.add_subplot(1, 2, 1, aspect='auto')
-    plot_attractor(ax1, img, color_map, extents, params)
+    render_trajectory_image(ax1, img, color_map, extents, params)
    
     ax2 = fig.add_subplot(1, 2, 2, aspect='auto')
     plot_hit_metrics(ax2, hit_metrics)
@@ -158,7 +158,7 @@ def get_validated_input(prompt, input_type=float, check_non_zero=False):
 
 
 def get_user_inputs():
-    # Collect input parameters from the user for attractor generation
+    # Collect input parameters from the user for hopalong attractor trajetory generation
     a = get_validated_input('Enter a non-zero float value for "a": ')#, float, check_non_zero=True)
     b = get_validated_input('Enter a float value for "b": ', float)
     c = get_validated_input('Enter a float value for "c": ', float)
@@ -167,21 +167,21 @@ def get_user_inputs():
     return a, b, c, num
 
 
-def generate_and_plot_attractor_and_statistics(a, b, c , num, image_size, color_map):
-    # Generate and process attractor points, compute metrics, and create visualizations
-    points = calculate_attractor_points(a, b, c, num)
+def generate_and_render_trajectory_image_and_statistics(a, b, c , num, image_size, color_map):
+    # simulate hopalong attractor trajetory, compute hit metrics, and create visualizations
+    points = hopalong_trajectory_simulation(a, b, c, num)
     img, extents, params, hit_metrics = prepare_plot_data(points, a, b, c, num, image_size)
     create_plots(img, extents, params, hit_metrics, color_map)
 
 
 def main():  
-    #Entry point: Coordinate user input, processing of attractor points and statistics and visualization generation
+    #Entry point: Coordinate user input, processing of attractor trajectory, hit metrics and visualization generation
     image_size = 1000, 1000
     color_map = 'hot'
 
     a, b, c, num = get_user_inputs()
 
-    generate_and_plot_attractor_and_statistics(a, b, c , num, image_size, color_map)
+    generate_and_render_trajectory_image_and_statistics(a, b, c , num, image_size, color_map)
 
 
 if __name__ == "__main__":

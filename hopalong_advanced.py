34608c3a190f48dc117facc4e1e8450d1be337df
Ralf <ratwolf@duck.com>
2024-07-11 13:22:33 +0200

    modified:   hopalong_advanced.py

--- hopalong_advanced.py
+++ hopalong_advanced.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numba import njit, prange
 from math import copysign, sqrt, fabs
-
     
+
 def get_user_inputs():
     # Request and validate user input with specified constraints
     def get_validated_input(prompt, input_type=float, check_non_zero=False, check_positive=False):
@@ -79,9 +79,13 @@ def render_trajectory_image(ax, img, extents, params, color_map):
     ax.imshow(img, origin="lower", cmap=color_map, extent=extents) # origin="lower" align according cartesian coordinates
     ax.set_title(
         "Hopalong Attractor@ratwolf@2024\nParams: a={a}, b={b}, c={c}, num={num:_}".format(**params))
+    
+    # Add Cartesian labels
+    ax.set_xlabel('X (Cartesian)')
+    ax.set_ylabel('Y (Cartesian)')
+    
 
-
-def calculate_hit_metrics(img):
+def calculate_hit_metrics(img, extents):
     # Analyze and summarize hit metrics from the hopalong trajectory image
     hit, count = np.unique(img[img > 0], return_counts=True)
     max_count_index = np.argmax(count)
@@ -90,8 +94,19 @@ def calculate_hit_metrics(img):
     count_for_max_hit = count[max_hit_index]
     hit_pixel = np.sum(count)
     img_pixels = np.prod(img.shape)
-    hit_ratio = '{:02.2f}'.format(hit_pixel / img_pixels * 100)
+    hit_ratio = '{:.2f}'.format(hit_pixel / img_pixels * 100)
 
+     # Find all pixels with the highest hit count
+    max_hit_count = np.max(img)
+    max_hit_coords = np.argwhere(img == max_hit_count)
+    
+    # Convert image coordinates pixels with the highest hit count to Cartesian coordinates
+    img_height, img_width = img.shape
+    min_x, max_x, min_y, max_y = extents
+    cartesian_x = min_x + (max_x - min_x) * (max_hit_coords[:, 1] / img_width)
+    cartesian_y = min_y + (max_y - min_y) * (max_hit_coords[:, 0] / img_height)
+    cartesian_coords = ['({:.3f}, {:.3f})'.format(x, y) for x, y in zip(cartesian_x, cartesian_y)]
+        
     hit_metrics = {
         "hit": hit,
         "count": count,
@@ -99,7 +114,8 @@ def calculate_hit_metrics(img):
         "count_for_max_hit": count_for_max_hit,
         "hit_pixel": hit_pixel,
         "img_points": img_pixels,
-        "hit_ratio": hit_ratio
+        "hit_ratio": hit_ratio,
+        "cartesian_coords": cartesian_coords
         }
     
     return hit_metrics
@@ -115,7 +131,8 @@ def plot_hit_metrics(ax, hit_metrics, scale='log'):
         f'Distribution of pixel hit count. \n'
         f'{hit_metrics["hit_pixel"]} pixels out of {hit_metrics["img_points"]} image pixels = {hit_metrics["hit_ratio"]}% have been hit. \n'
         f'The highest number of pixels with the same number of hits is {np.max(hit_metrics["count"])} with {hit_metrics["hit_for_max_count"]} hits. \n'
-        f'The highest number of hits is {np.max(hit_metrics["hit"])} with {hit_metrics["count_for_max_hit"]} pixels hit.')
+        f'The highest number of hits is {np.max(hit_metrics["hit"])} with {hit_metrics["count_for_max_hit"]} pixels hit\n'
+        f'Cartesian coordinates for highest #of hits:{hit_metrics["cartesian_coords"]}')
     
     ax.set_title(title_text, fontsize=10)
     ax.set_xscale(scale)
@@ -150,7 +167,7 @@ def main(image_size=(1000, 1000), color_map='hot'):
 
     img, extents = generate_trajectory_image(points, image_size)
     
-    hit_metrics = calculate_hit_metrics(img) 
+    hit_metrics = calculate_hit_metrics(img, extents)
 
     visualize_trajectory_image_and_hit_metrics(img, extents, params, color_map, hit_metrics)
 

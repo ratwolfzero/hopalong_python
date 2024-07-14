f48a890814feaac2922f11de65cd6f5e5690f87b
Ralf <ratwolf@duck.com>
2024-07-14 10:12:58 +0200

    modified:   hopalong_advanced.py

--- hopalong_advanced.py
+++ hopalong_advanced.py
@@ -56,7 +56,7 @@ def compute_trajectory(a, b, c, num):
     return points
 
 
-@njit(parallel=True)
+@njit('Tuple((uint32[:,:], float32[:]))(float32[:,:], Tuple((uint64, uint64)))', parallel=True)
 def generate_trajectory_image(points, image_size):
     # Generates an image array with the mapped trajectory points
     img_width, img_height = image_size
@@ -76,7 +76,7 @@ def generate_trajectory_image(points, image_size):
         # populate image array, respect the row-column (y-x) indexing
         image[py[i], px[i]] += 1
 
-    extents = [min_x, max_x, min_y, max_y]
+    extents = np.array([min_x, max_x, min_y, max_y], dtype=np.float32)
 
     return image, extents
 
@@ -161,10 +161,7 @@ def main(image_size=(1000, 1000), color_map='hot'):
     Generate Hopalong Attractor and hit metrics: Get user inputs, compute hopalong trajectory, generate trajectory image.
     Calculate hit metrics, visualize trajectory image and hit metrics
     """
-    # dummy (pre-)compilation of @njit decorated functions
-    _ = compute_trajectory(0.0, 0.0, 0.0, 1) 
-    _ = generate_trajectory_image(np.zeros((1, 2), dtype=np.float32), (1, 1))
-
+    
     a, b, c, num, params = get_user_inputs()
 
     points = compute_trajectory(a, b, c, num)

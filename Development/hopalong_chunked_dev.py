191bf0519c28ae635711813450869c6347a55d1d
Ralf <ratwolf@duck.com>
2024-07-29 15:46:15 +0200

    modified:   Development/hopalong_chunked_dev.py

--- Development/hopalong_chunked_dev.py
+++ Development/hopalong_chunked_dev.py
@@ -69,7 +69,7 @@ def create_chunks(num, chunk_size):
 @njit
 def compute_trajectory_chunk(a, b, c, current_chunk_size, x0, y0):
     """Compute a chunk of the Hopalong trajectory."""
-    points = np.empty((current_chunk_size, 2), dtype=np.float64)
+    points = np.zeros((current_chunk_size, 2), dtype=np.float64)
     x, y = x0, y0
     for i in range(current_chunk_size):
         points[i] = x, y

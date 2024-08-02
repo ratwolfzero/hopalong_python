c1c92929630fe07e3ec740c41b7aa2374c237d9f
Ralf <ratwolf@duck.com>
2024-08-02 04:04:43 +0200

    modified:   Development/current_develoment/hopalong_chunked_dev.py

--- Development/current_develoment/hopalong_chunked_dev.py
+++ Development/current_develoment/hopalong_chunked_dev.py
@@ -8,7 +8,7 @@ from math import copysign, sqrt, fabs
 
 
 def get_validated_input(prompt, input_type=float, check_non_zero=False, check_positive=False):
-    """Prompt for and return user input validated by type and positive/non-zero checks."""
+    #Prompt for and return user input validated by type and positive/non-zero checks
     while True:
         user_input = input(prompt)
         try:
@@ -25,7 +25,7 @@ def get_validated_input(prompt, input_type=float, check_non_zero=False, check_po
 
 
 def get_attractor_parameters():
-    """Prompt user to input parameters for the Hopalong Attractor."""
+    #Prompt user to input parameters for the Hopalong Attractor
     a = get_validated_input('Enter a float value for "a": ', float)
     b = get_validated_input('Enter a float value for "b": ', float)
     c = get_validated_input('Enter a float value for "c": ', float)
@@ -56,7 +56,7 @@ def compute_full_trajectory_extents(a, b, c, num):
 
 @njit
 def generate_chunk_sizes(num, chunk_size):
-    """Yield sizes of chunks to process in each iteration until covering the entire range."""
+    #Yield sizes of chunks to process in each iteration until covering the entire range
     for i in range(0, num, chunk_size):
         current_chunk_size = min(chunk_size, num - i)
         yield current_chunk_size
@@ -64,13 +64,13 @@ def generate_chunk_sizes(num, chunk_size):
 
 @njit
 def compute_trajectory_chunk(a, b, c, current_chunk_size, x0, y0):
-    """Compute a chunk of the Hopalong trajectory."""
+    #Compute a chunk of the Hopalong trajectory
     points = np.zeros((current_chunk_size, 2), dtype=np.float64)
     x, y = x0, y0
     for i in range(current_chunk_size):
         points[i] = x, y
         xx = y - copysign(1.0, x) * sqrt(fabs(b * x - c))
-        """signum function respecting the behavior of floating point numbers according to IEEE 754 (signed zero)"""
+        #signum function respecting the behavior of floating point numbers according to IEEE 754 (signed zero)
         yy = a - x
         x, y = xx, yy
     return points, x, y
@@ -78,21 +78,21 @@ def compute_trajectory_chunk(a, b, c, current_chunk_size, x0, y0):
 
 @njit(parallel=True)
 def map_trajectory_chunk_to_image(image, points, extents):
-    """Map trajectory chunk points to image pixel locations and populate the image accordingly."""
+    #Map trajectory chunk points to image pixel locations and populate the image accordingly
     min_x, max_x, min_y, max_y = extents
     img_width, img_height = image.shape[1], image.shape[0]
     px = ((points[:, 0] - min_x) / (max_x - min_x) * (img_width - 1)).astype(np.uint64)
     py = ((points[:, 1] - min_y) / (max_y - min_y) * (img_height - 1)).astype(np.uint64)
     for i in prange(len(px)): 
-        """use of prange for parallel loop"""
+        #use of prange for parallel loop
         if 0 <= px[i] < img_width and 0 <= py[i] < img_height:
             image[py[i], px[i]] += 1
-        """populate image array, respect the row-column (y-x) indexing"""    
+        #populate image array, respect the row-column (y-x) indexing   
 
 
 @njit(parallel=True)
 def compute_full_trajectory_image(a, b, c, num, chunk_size, extents, image_size):
-    """Calculate the full trajectory image from chunks."""
+    #Calculate the full trajectory image from chunks
     img_width, img_height = image_size
     image = np.zeros((img_height, img_width), dtype=np.uint64)
     x0 = y0 = np.float64(0)
@@ -103,17 +103,17 @@ def compute_full_trajectory_image(a, b, c, num, chunk_size, extents, image_size)
 
 
 def render_full_trajectory_image(image, extents, params, color_map):
-    """Render the full trajectory image using matplotlib."""
+    #Render the full trajectory image
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(1, 1, 1, aspect='auto')
-    """origin="lower" align according cartesian coordinates"""
+    #origin="lower" align according cartesian coordinates
     ax.imshow(image, origin="lower", cmap=color_map, extent=extents)
     ax.set_title("Hopalong Attractor@ratwolf@2024\nParams: a={a}, b={b}, c={c}, num={num:_}".format(**params))
     plt.show()
 
 
 def main(image_size=(1000, 1000), color_map='hot', chunk_size=1048576):
-    """Execute processes to generate and render the Hopalong Attractor."""
+    #Execute processes to generate and render the Hopalong Attractor
     try:
         a, b, c, num, params = get_attractor_parameters()
         extents = compute_full_trajectory_extents(a, b, c, num)

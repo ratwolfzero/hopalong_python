62ac7b684f749d870fd281d29d6c4c8498f16cff
Ralf <ratwolf@duck.com>
2024-08-22 06:22:42 +0200

    modified:   Development/experimental/hopalong_singularity.py

--- Development/experimental/hopalong_singularity.py
+++ Development/experimental/hopalong_singularity.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numba import njit
 from math import copysign
 
 
 # Parameters for the Hopalong attractor
 a = 1
-b = 2
-c = 3
+b = 1
+c = 1
 
 # Number of iterations
-iterations = 100
+iterations = 10000
 # Initialize arrays to hold the points
 x = np.zeros(iterations)
 y = np.zeros(iterations)
@@ -29,23 +30,23 @@ def custom_sign(x):
 
 # Iterate the Hopalong attractor equations
 for n in range(iterations - 1):
-    x[n + 1] = y[n] - np.sign(x[n]) * np.sqrt(abs(b * x[n] - c))
-    x[n + 1] = y[n] - custom_sign(x[n]) * np.sqrt(abs(b * x[n] - c))
-    x[n + 1] = y[n] - copysign(1.0, x[n])  * np.sqrt(abs(b * x[n] - c))
+    #x[n + 1] = y[n] - np.sign(x[n]) * np.sqrt(abs(b * x[n] - c))
+    #x[n + 1] = y[n] - custom_sign(x[n]) * np.sqrt(abs(b * x[n] - c))
+    x[n + 1] = y[n] - copysign(1.0, x[n]) * np.sqrt(abs(b * x[n] - c))
     y[n + 1] = a - x[n]
 	
 	
-print(x)
-print(y)
+#print(x)
+#print(y)
 
 # Plot the results
-"""
+
 plt.figure(figsize=(8, 8))
 plt.plot(x, y, 'o', c='red', markersize=0.2)
 plt.title(f'Hopalong Attractor with a={a}, b={b}, c={c}')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
-"""
+
 
 

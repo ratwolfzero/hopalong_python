e905d62bb2c63e94b309a77523ea2fcf7f4d71cb
Ralf <ratwolf@duck.com>
2024-11-14 22:39:26 +0100

    modified:   Check_Periodic_Orbits/hopalong_periodic_orbits.py

--- Check_Periodic_Orbits/hopalong_periodic_orbits.py
+++ Check_Periodic_Orbits/hopalong_periodic_orbits.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import copysign
-
+from numba import njit
 
 # Parameters for the Hopalong attractor
 a = 0.6
@@ -10,6 +9,7 @@ c = 0
 
 # Number of iterations
 iterations = 1000000
+
 # Initialize arrays to hold the points
 x = np.zeros(iterations)
 y = np.zeros(iterations)
@@ -18,8 +18,8 @@ y = np.zeros(iterations)
 x[0] = 0.0
 y[0] = 0.0
 
+@njit
 def custom_sign(x):
-    
     if np.isnan(x):
         return np.nan
     elif x > 0 or x == 0.0:
@@ -27,25 +27,26 @@ def custom_sign(x):
     else:
         return -1.0
 
-# Iterate the Hopalong attractor equations
-for n in range(iterations - 1):
-    x[n + 1] = y[n] - np.sign(x[n]) * np.sqrt(abs(b * x[n] - c))
-    #x[n + 1] = y[n] - custom_sign(x[n]) * np.sqrt(abs(b * x[n] - c))
-    #x[n + 1] = y[n] - copysign(1.0, x[n]) * np.sqrt(abs(b * x[n] - c))
-    y[n + 1] = a - x[n]
-	
-	
-#print(x)
-#print(y)
+@njit
+def compute_hopalong(x, y, a, b, c, iterations):
+    # Iterate the Hopalong attractor equations
+    for n in range(iterations - 1):
+        x[n + 1] = y[n] - np.sign(x[n]) * np.sqrt(abs(b * x[n] - c))
+        y[n + 1] = a - x[n]
+    return x, y
 
 # Plot the results
+def plot_hopalong(x, y):
+    plt.figure(figsize=(8, 8))
+    plt.plot(x, y, 'o', c='red', markersize=5)
+    plt.title(f'Hopalong Attractor with a={a}, b={b}, c={c}')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
 
-plt.figure(figsize=(8, 8))
-plt.plot(x, y, 'o', c='red', markersize=5)
-plt.title(f'Hopalong Attractor with a={a}, b={b}, c={c}')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+def main():
+    # Call the compute_hopalong function with arguments
+    x_vals, y_vals = compute_hopalong(x, y, a, b, c, iterations)
+    plot_hopalong(x_vals, y_vals)
 
-
-
+main()

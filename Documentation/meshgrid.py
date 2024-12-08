99b5af29ae9347380055cdb7679cc0e64c63d043
Ralf <ratwolf@duck.com>
2024-12-08 18:18:27 +0100

    new file:   Documentation/meshgrid.py deleted:    Documentation/np_meshgrid.md deleted:    Documentation/np_meshgrid.png

---
+++ Documentation/meshgrid.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_edges = np.array([1, 2, 3])
+y_edges = np.array([4, 5, 6])
+
+X, Y = np.meshgrid(x_edges, y_edges)
+
+fig = plt.figure(figsize=(8, 8),facecolor='gainsboro')
+plt.plot(X, Y, 'o', c='blue', markersize=8)
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
+
+
